a36b5606fecfc32e1cd7bd15612841d1079da8e6
Scott Pidzarko <scott.pidzarko@gmail.com>
2016-07-27 15:51:50 -0700

    Making the web frontend use the location for the audio given in the ini file as opposed to being hardcoded

--- web_root/py/arch.py
+++ web_root/py/arch.py
@@ -6,6 +6,7 @@ from time import mktime
 import sys
 import tempfile
 import os
+import configparser
 
 #####
 # CiTR Radio Archive Access
@@ -18,6 +19,11 @@ import os
 # ###### #####   #   ## ##
 #######
 
+#enter the location of the the config.ini file. This default should work:
+configLocation = os.normpath("../../citrlog/config.ini")
+#Load the config
+settings = configparser.ConfigParser()
+config = settings.read(configLocation)
 
 def joinMP3s(files):
 	#output = ##find output file name
@@ -38,7 +44,7 @@ class ArchiveJoiner:
 		return "%s/%d/%d/%d" % (self.getBase(), year, month, day)
 	def getFile(self, time):
 		seconds = int(mktime(time.timetuple()))
-		return "%s/%d-second.mp3" % (self.getPath(time.year, time.month, time.day), 
+		return "%s/%d-second.mp3" % (self.getPath(time.year, time.month, time.day),
 					     seconds)
 	def __init__(self, startTime, endTime, base, empty=None):
 		#if empty has a value, insert that for every blank second
@@ -60,46 +66,42 @@ class ArchiveJoiner:
 
 	def getMP3(self):
 		return joinMP3s(self.__files)
-	
 
 def main():
-        __startTime = datetime.strptime("21-5-2013 15:00:00", "%d-%m-%Y %H:%M:%S")
+    __startTime = datetime.strptime("21-5-2013 15:00:00", "%d-%m-%Y %H:%M:%S")
 	print __startTime
-        __endTime = datetime.strptime("21-5-2013 16:00:00", "%d-%m-%Y %H:%M:%S")
-        aj = ArchiveJoiner(__startTime, __endTime)
-        mp3 = aj.getMP3()
+    __endTime = datetime.strptime("21-5-2013 16:00:00", "%d-%m-%Y %H:%M:%S")
+    aj = ArchiveJoiner(__startTime, __endTime)
+    mp3 = aj.getMP3()
 	print "mp3 is %s" % (mp3)
-	
 
 if __name__ == "__main__":
     main()
-			
-		
 
 def outputPSP(req, n, variables):
 	req.content_type = "text/html"
-        p = psp.PSP(req, "psp/%s.psp" % (n),variables)
-        p.run()
+    p = psp.PSP(req, "psp/%s.psp" % (n),variables)
+    p.run()
 
 def download(req,fname):
 	req.content_type = "audio/mpeg"
 	req.sendfile("/tmp/%s.mp3" % (fname))
 
-def download(req,startTime,endTime,archive):
+def download(req,startTime,endTime):
 	__startTime = datetime.strptime(startTime, "%d-%m-%Y %H:%M:%S")
 	__endTime = datetime.strptime(endTime, "%d-%m-%Y %H:%M:%S")
+
+    #Read the audiologbase from config.ini
+    archive = config.get('Main', 'audiologbase')
 	aj = ArchiveJoiner(__startTime, __endTime, archive)
 	mp3 = aj.getMP3()
 	f = os.stat(mp3)
 	req.set_content_length(f.st_size)
 	req.headers_out['Content-Disposition'] = "attachment; filename=archive.mp3"
 	req.sendfile(mp3)
-	
+
 def archive(req,starTime,hours,minutes,seconds):
 	arch = ArchiveRequest(startTime, hours, minutes, seconds)
 
 def index(req,startTime="17-04-2013 00:00:00"):
 	outputPSP(req, "index", {})
-
-
-
